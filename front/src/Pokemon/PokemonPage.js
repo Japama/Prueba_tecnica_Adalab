ffa23a6a330843c39d53c410a4ff08b42d7d0f90
Juan Bautista Valero Carrasco <jbvc91@gmail.com>
2023-12-14 18:16:19 +0100

    Se ha creado el listado de los pokemon obtenidos del back

--- front/src/Pokemon/PokemonPage.js
+++ front/src/Pokemon/PokemonPage.js
@@ -6,7 +6,6 @@ import './PokemonPage.sass'; // Importa el archivo SCSS aquí
 function PokemonPage({ children }) {
     return (
         <div className="content">
-            <h1> Pokedex </h1>
             <div className="triangle top-left"></div>
             <div className="triangle top-right"></div>
             <div className="circle bottom-left"></div>
