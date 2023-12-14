d9bf6c38d393fc3fffc2135423b504538fcbb85c
Juan Bautista Valero Carrasco <jbvc91@gmail.com>
2023-12-14 14:31:46 +0100

    Se ha habilitado el CORS para permitir las llamadas del front

--- back/src/app.js
+++ back/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pokemonRoutes = require('./api/routes/pokemonRoutes');
-
+const cors = require('cors');
 const app = express();
-app.use(bodyParser.json());
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use('/api', pokemonRoutes);
 
 const port = process.env.PORT || 3003;
