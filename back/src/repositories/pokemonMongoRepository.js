737242e1b379cdde358a38ab8e62d123f9474bc0
Juan Bautista Valero Carrasco <jbvc91@gmail.com>
2023-12-17 11:37:29 +0100

    Se ha añadido un método para filtrar mediante MongoDB

--- back/src/repositories/pokemonMongoRepository.js
+++ back/src/repositories/pokemonMongoRepository.js
@@ -26,7 +26,20 @@ const getPokemonById = async (id) => {
 };
 
 
+const getPokemonFilter = async (filter) => {
+    try {
+        const db = getDb();
+        const collection = db.collection('Pokemons');
+        const pokemon = await collection.find({ Name: new RegExp(filter, 'i') }).toArray();
+        return pokemon;
+    } catch (err) {
+        console.error('Error al obtener el Pokemon:', err);
+        throw err; // Propaga el error para manejarlo en el controlador
+    }
+};
+
 module.exports = {
     getPokemonList,
-    getPokemonById
+    getPokemonById,
+    getPokemonFilter
 };
